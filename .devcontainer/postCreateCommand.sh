6dc39515ed66ad459e8ff3efe656d53e04dc793e
Rich Thanki <richardthanki@gmail.com>
2026-01-12 00:35:02 +0000

    revise docker image tooling, test using correct trie version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,32 +1,19 @@
 #!/bin/sh
 
-sudo apt update -y
-
-sudo apt install -y apt-utils unzip curl wget git build-essential libreadline-dev dialog libssl-dev m4 lua-cjson
-
-# install core lua packages
-
-sudo apt install -y lua5.1 liblua5.1-dev luajit luarocks
-
-# install luarocks packages
-
-sudo luarocks install bit32
-sudo luarocks install cqueues
-sudo luarocks install http
-sudo luarocks install luaposix
-
-# install cffi-lua
-
-sudo apt install -y meson pkg-config cmake libffi-dev
+cd /tmp
+git clone https://github.com/jangala-dev/lua-fibers
+cd lua-fibers
+git checkout v0.1
+sudo mv fibers /usr/local/share/lua/5.1/
+cd ..
+rm -rf lua-fibers
 
 cd /tmp
-sudo rm -rf cffi-lua
-git clone https://github.com/q66/cffi-lua
-mkdir cffi-lua/build
-cd cffi-lua/build
-sudo meson .. -Dlua_version=5.1 --buildtype=release
-sudo ninja all
-sudo ninja test
-sudo cp cffi.so /usr/local/lib/lua/5.1/cffi.so
+git clone https://github.com/jangala-dev/lua-trie
+cd lua-trie
+git checkout v0.1
+sudo mv src/trie.lua /usr/local/share/lua/5.1/
+cd ..
+rm -rf lua-trie
 
 exit 0
